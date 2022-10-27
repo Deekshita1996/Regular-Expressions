1f4b77e6c5434faac9397f36762af2084e0e520b
Deekshita1996 <113499307+Deekshita1996@users.noreply.github.com>
2022-10-27 10:39:48 +0530

    Need to enter a valid Password at least 1 Special character
    Need to enter a valid Password at least 1 Special character

---
+++ PasswordUC_8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPatternMatchingDemo
+{
+    public class PasswordUC_8
+    {
+        public static string REGEX_PASSWORDUC_8 = "[a-z]{7}[#]{1}$";
+        public bool validatePasswordUC_8(string passwordUC_8)
+        {
+            return Regex.IsMatch(passwordUC_8, REGEX_PASSWORDUC_8);
+        }
+    }
+}
+
