ca39c89f1932df98960a827b445eab1febbb2f28
Deekshita1996 <113499307+Deekshita1996@users.noreply.github.com>
2022-10-27 10:35:00 +0530

    Need to enter a valid Password at least 1 Uppercase
    Need to enter a valid Password at least 1 Uppercase

---
+++ PasswordUC_6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPatternMatchingDemo
+{
+    public class PasswordUC_6
+    {
+        public static string PASSWORDUC_6_REGEX = "[A-Z]{1}[a-z]{7}$";
+        public bool validatePasswordUC_6(string passwordUC_6)
+        {
+            return Regex.IsMatch(passwordUC_6, PASSWORDUC_6_REGEX);
+        }
+    }
+}
+
